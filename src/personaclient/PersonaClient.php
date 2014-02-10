00f2e43ce53c380ea4a1e8a5efc09a88398c53ad
Nadeem Shabir <nadeem.shabir@talis.com>
2014-02-10 11:31:21 +0000

    added predis dependency because I need a php redis client library, also added basic unit tests

--- src/personaclient/PersonaClient.php
+++ src/personaclient/PersonaClient.php
@@ -3,5 +3,39 @@ namespace personaclient;
 
 class PersonaClient {
 
+    public function __construct($config) {
+        $this->checkConfig($config);
+    }
 
+    /* Protected functions */
+
+
+    protected function checkConfig($config){
+        if(empty($config)){
+            throw new \InvalidArgumentException("No config provided to Persona Client");
+        }
+
+        $requiredProperties = array(
+            'persona_host',
+            'persona_port',
+            'persona_scheme',
+            'persona_oauth_route',
+            'tokencache_redis_host',
+            'tokencache_redis_port',
+            'tokencache_redis_db'
+        );
+
+        $missingProperties = array();
+        foreach($requiredProperties as $property){
+            if(!isset($config[$property])){
+                array_push($missingProperties, $property);
+            }
+        }
+
+        if(empty($missingProperties)){
+            return true;
+        } else {
+            throw new \InvalidArgumentException("Config provided does not contain values for: " . implode(",", $missingProperties));
+        }
+    }
 }
--- test/unit/PersonaClientTest.php
+++ test/unit/PersonaClientTest.php
@@ -10,8 +10,32 @@ require_once $appRoot . '/test/unit/TestBase.php';
 
 class PersonaClientTest extends TestBase {
 
-    function testToDo(){
-        $personaClient = new \personaclient\PersonaClient();
-        $this->assertTrue(true);
+    function testEmptyConfigThrowsException(){
+        $this->setExpectedException('InvalidArgumentException',
+            'No config provided to Persona Client'
+        );
+        $personaClient = new \personaclient\PersonaClient(array());
+    }
+
+    function testNullConfigThrowsException(){
+        $this->setExpectedException('InvalidArgumentException',
+            'No config provided to Persona Client'
+        );
+        $personaClient = new \personaclient\PersonaClient(null);
+    }
+
+    function testMissingRequiredConfigParamsThrowsException(){
+        $this->setExpectedException('InvalidArgumentException',
+            'Config provided does not contain values for: persona_host,persona_port,persona_scheme,persona_oauth_route,tokencache_redis_host,tokencache_redis_port,tokencache_redis_db'
+        );
+        $personaClient = new \personaclient\PersonaClient(array(
+            'persona_host' => null,
+            'persona_port' => null,
+            'persona_scheme' => null,
+            'persona_oauth_route' => null,
+            'tokencache_redis_host' => null,
+            'tokencache_redis_port' => null,
+            'tokencache_redis_db' => null,
+        ));
     }
 }
